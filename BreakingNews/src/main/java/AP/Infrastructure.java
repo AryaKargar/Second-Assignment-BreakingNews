564f354445622f3fbf5c4d68c4b423a31770177e
arya <aryakargar85@gmail.com>
2025-03-23 22:11:37 +0330

    last bug fixes

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -23,6 +23,7 @@ public class Infrastructure {
     public Infrastructure(String APIKEY) {
         this.APIKEY = APIKEY;
         this.URL = "https://newsapi.org/v2/everything?q=tesla&from=" + LocalDate.now().minusDays(1) + "&sortBy=publishedAt&apiKey=";
+//        this.URL = "https://newsapi.org/v2/everything?q=tesla&from=2025-02-05&sortBy=publishedAt&apiKey=";
         this.JSONRESULT = getInformation();
         parseInformation();
     }
